47b87e9519392c9cdb42a64903af2a3beb4f7d8b
Gustavo Anderson <68356643+gustavoanderson@users.noreply.github.com>
2026-08-24 16:04:32 -0300

    Update cypress.config.js

--- tic-tac-toe/cypress.config.js
+++ tic-tac-toe/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "pzgdmc",
   e2e: {
     // Aponta para um servidor estático local servindo a pasta tic-tac-toe/.
     // Veja o README.md desta pasta para como subir esse servidor antes de rodar os testes.
